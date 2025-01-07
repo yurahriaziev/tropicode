ca2a4c2271ec94732a713c6d69cfbebdc1e47d34
yurahriaziev <yura.hriaziev@gmail.com>
2025-01-07 11:34:11 -0500

    SITE LIVE

--- backend/config.py
+++ backend/config.py
@@ -1,11 +1,8 @@
 from flask import Flask
 from flask_cors import CORS
-# from flask_session import Session
 import os
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
 
-# app.config['SESSION_TYPE'] = 'filesystem'
-# Session(app)
 CORS(app, resources={r"/*": {"origins": {"http://localhost:3000", "https://yurahriaziev.github.io"}}})
