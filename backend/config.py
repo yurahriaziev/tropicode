7df33fc156eef59e2e34ec54e86e1910fa28df9b
yurahriaziev <yura.hriaziev@gmail.com>
2025-02-13 08:23:43 -0500

    fixes for production

--- backend/config.py
+++ backend/config.py
@@ -10,15 +10,15 @@ app.secret_key = os.urandom(24)
 load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 app.config['SECRET_KEY'] = SECRET_KEY
-# app.config['SESSION_TYPE'] = 'redis'
+app.config['SESSION_TYPE'] = 'redis'
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SESSION_PERMANENT'] = False
 app.config['SESSION_USE_SIGNER'] = True
 app.config['SESSION_KEY_PREFIX'] = 'oauth_'
 app.config['SESSION_REDIS'] = redis.StrictRedis(host='localhost', port=6379, db=1, decode_responses=True)
 
-# production_url = "https://www.tropicode.tech"
-production_url = "http://localhost:3000"
+production_url = "https://www.tropicode.tech"
+# production_url = "http://localhost:3000"
 
 CORS(app, resources={r"/*": {"origins": {"http://localhost:3000", "https://yurahriaziev.github.io", "https://www.tropicode.tech"}}})
 
