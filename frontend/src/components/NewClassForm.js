5a3d933b6cf38c48f0e5db550b21b3d1581c8a78
yurahriaziev <yura.hriaziev@gmail.com>
2025-02-17 11:21:41 -0500

    new class form fixes

--- frontend/src/components/NewClassForm.js
+++ frontend/src/components/NewClassForm.js
@@ -49,7 +49,7 @@ export default function NewClassForm({ handleNewClassClick, setError, createNewM
     // for debugging
     const startTimeOptions = []
     for (let hour = 16; hour <= 18; hour++) {
-        for (let min = 0; min < 60; min+=1) {
+        for (let min = 0; min < 60; min+=5) {
             if (hour === 18 && min > 0) break
             const formatedTime = `${hour.toString().padStart(2, "0")}:${min.toString().padStart(2, "0")}`
             startTimeOptions.push(formatedTime)
@@ -57,7 +57,7 @@ export default function NewClassForm({ handleNewClassClick, setError, createNewM
     }
     const endTimeOptions = []
     for (let hour = 17; hour <= 19; hour++) {
-        for (let min = 0; min < 60; min+=1) {
+        for (let min = 0; min < 60; min+=5) {
             if (hour === 19 && min > 0) break
             const formatedTime = `${hour.toString().padStart(2, "0")}:${min.toString().padStart(2, "0")}`
             endTimeOptions.push(formatedTime)
