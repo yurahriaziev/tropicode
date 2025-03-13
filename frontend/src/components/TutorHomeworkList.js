a04f1dfd2d3ba347230bfcb4aad670e2c023a0b1
yurahriaziev <yura.hriaziev@gmail.com>
2025-03-13 11:09:09 -0400

    tutor homework sorted

--- frontend/src/components/TutorHomeworkList.js
+++ frontend/src/components/TutorHomeworkList.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Homework from "./Homework";
 
 export default function TutorHomeworkList({ assignedHomework, handleRemoveHomework, setError }) {
+    const [sortedHomework, setSortedHomework] = useState([])
+
+    useEffect(() => {
+        const sortHomework = () => {
+            const sorted = [...assignedHomework].sort((a, b) => new Date(b.dueDate) - new Date(a.dueDate))
+            setSortedHomework(sorted)
+        }
+
+        sortHomework()
+    }, [assignedHomework])
+
     return (
         <div>
-            {assignedHomework.length > 0 ? (
+            {sortedHomework.length > 0 ? (
                 <ul>
-                    {assignedHomework.map((homeworkData, index) => (
+                    {sortedHomework.map((homeworkData, index) => (
                         <Homework key={index} index={index} homeworkData={homeworkData} view='tutor' handleRemoveHomework={handleRemoveHomework} setError={setError} />
                     ))}
                 </ul>
