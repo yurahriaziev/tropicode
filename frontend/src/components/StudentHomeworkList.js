350ff00c2d7a63a74e7f10cf74329b9744f956c0
yurahriaziev <yura.hriaziev@gmail.com>
2025-03-12 22:09:06 -0400

    homework sorted

--- frontend/src/components/StudentHomeworkList.js
+++ frontend/src/components/StudentHomeworkList.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Homework from "./Homework";
 
 export default function StudentHomeworkList({ homeworks, setError, setSuccess }) {
+    const [sortedHomework, setSortedHomework] = useState([])
+
+    useEffect(() => {
+        const sortByDate = () => {
+            const sorted = [...homeworks].sort((a, b) => new Date(b.dueDate) - new Date(a.dueDate))
+            setSortedHomework(sorted)
+        }
+
+        sortByDate()
+    }, [homeworks])
+
+    // console.log(homeworks)
     return (
         <div>
-            {homeworks.length > 0 ? (
+            {sortedHomework.length > 0 ? (
                 <ul>
-                    {homeworks.map((homeworkData, index) => (
+                    {sortedHomework.map((homeworkData, index) => (
                         <Homework key={index} homeworkData={homeworkData} view='student' setError={setError} setSuccess={setSuccess} />
                     ))}
                 </ul>
