486b950c94951f0a8bee7024d6c1b363d468e2d1
yurahriaziev <yura.hriaziev@gmail.com>
2025-03-19 09:11:41 -0400

    fix

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,4 @@
 export const API_BASE_URL = 'https://api.tropicode.tech'
+// commit to fix api
 // export const API_BASE_URL = 'http://127.0.0.1:5001'
 export const PYTHON_RUN_API = 'https://3t4qqrulob.execute-api.us-east-1.amazonaws.com/OriginalDeployment/RunPythonCode'
